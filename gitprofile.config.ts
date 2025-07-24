2713333265c7800b07dde538394d1cac092699fb
Derşan Onur <65139156+onurdersan@users.noreply.github.com>
2025-07-24 17:58:34 +0300

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -39,6 +39,7 @@ const CONFIG = {
        },
       ],
     },
+    },
   seo: { title: 'Derşan Onur', description: 'Pediatrician, M.D.', imageURL: '' },
   social: {
     linkedin: 'dersanonur',
@@ -167,22 +168,25 @@ const CONFIG = {
   ],
   publications: [
     {
-      title: 'Publication Title',
+      title: 'Pediatrics 4.0: the Transformative Impacts of the Latest Industrial Revolution on Pediatrics',
       conferenceName: '',
-      journalName: 'Journal Name',
-      authors: 'John Doe, Jane Smith',
-      link: 'https://example.com',
+      journalName: 'Health Care Analysis',
+      authors: 'Derşan Onur, Çağla Özbakır',
+      link: 'https://doi.org/10.1007/s10728-025-00536-z',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'Industry 4.0 represents the latest phase of industrial evolution, characterized by the seamless integration of cyber-physical systems, the Internet of Things, big data analytics, artificial intelligence, advanced robotics, and cloud computing, enabling smart, adaptive, and interconnected processes where physical, digital, and biological realms converge. In parallel, healthcare has progressed from the traditional, physician-centered model of Healthcare 1.0 by introducing medical devices and digitized records to Healthcare 4.0, which leverages Industry 4.0 technologies to create personalized, data-driven, and patient-centric systems. In this context, we hereby introduce Pediatrics 4.0 as a new paradigm that adapts these innovations to children’s unique developmental, physiological, and ethical considerations and aims to improve diagnostic precision, treatment personalization, and continuous monitoring in pediatric populations. Key applications include AI-driven diagnostic and predictive analytics, IoT-enabled remote monitoring, big data-powered epidemiological insights, robotic assistance in surgery and rehabilitation, and 3D printing for patient-specific devices and pharmaceuticals. However, realizing Pediatrics 4.0 requires addressing significant challenges—data privacy and security, algorithmic bias, interoperability and standardization, equitable access, regulatory alignment, the ethical complexities of consent, and long-term technology exposure. Future research should focus on explainable AI, pediatric-specific device design, robust data governance frameworks, dynamic ethical and legal guidelines, interdisciplinary collaboration, and workforce training to ensure these transformative technologies translate into safer, more effective, and more equitable child healthcare.',
     },
     {
       title: 'Publication Title',
       conferenceName: 'Conference Name',
       journalName: '',
-      authors: 'John Doe, Jane Smith',
-      link: 'https://example.com',
+      authors: 'Derşan Onur, Sunanur Çiftçi Sadıkoğlu, Nilgün Harputluoğlu, Behzat Özkan',
+      link: 'https://doi.org/10.1186/s12904-024-01546-9',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'Background: Pediatric palliative care (PPC) patients are at an elevated risk of malnutrition. Nutritional inadequacy can also cause micronutrient deficiencies. These factors can lead to weight loss, stunted growth, and poor quality of life. Despite the prevalence of these issues, limited research exists in the micronutrient status of PPC patients. The purpose of this study was to determine the vitamin B12 and D, iron, ferritin, folate, calcium, phosphorus, and magnesium levels of PPC patients to contribute to a better understanding of their micronutrient needs as well as the appropriate management of diet and treatment approaches.
+        Methods: This was a single-center observational cross-sectional retrospective study. This study evaluated the levels of vitamin B12, 25-hydroxyvitamin D, iron, ferritin, folate, calcium, phosphorus, and magnesium in PPC patients. The patients were classified according to the Chronic Complex Conditions (CCC) v2 and then compared.
+        Results: A total of 3,144 micronutrient data points were collected from 822 hospitalizations of 364 patients. At least one micronutrient deficiency was identified in 96.9% of the patients. The most prevalent deficiencies were observed for iron, calcium, and phosphate. In addition, 25-hydroxyvitamin D deficiency was observed in one-third of patients. Calcium, magnesium, phosphorus, folate, and 25-hydroxyvitamin D were negatively correlated with age.
+        Conclusion: The results of this study indicate that micronutrient deficiencies are highly prevalent in PPC patients. These findings have the potential to contribute to improvements in the nutritional and therapeutic management of patients.',
     },
   ],
   // Display articles from your medium or dev account. (Optional)
