419ed1fd120b6ea64a4bdef7493395956f2c90fc
Derşan Onur <65139156+onurdersan@users.noreply.github.com>
2025-07-24 20:22:51 +0300

    Update index.tsx

--- src/components/scholar-card/index.tsx
+++ src/components/scholar-card/index.tsx
@@ -2,9 +2,8 @@
 
 import { Fragment } from 'react';
 import { PiBooks } from 'react-icons/pi';
-import scholarData from '../../data/scholar.json'; // Veriyi direkt dosyadan import ediyoruz
+import { skeleton } from '../../utils';
 
-// Interface'i config dosyasına taşıyabilir veya burada tutabiliriz
 interface ScholarArticle {
   title: string;
   link: string;
@@ -15,11 +14,26 @@ interface ScholarArticle {
   };
 }
 
-const ScholarCard = () => {
-  const articles: ScholarArticle[] = scholarData.articles;
+const ScholarCard = ({
+  articles,
+  loading,
+}: {
+  articles: ScholarArticle[];
+  loading: boolean;
+}) => {
+  const renderSkeleton = () => {
+    return Array.from({ length: 2 }).map((_, index) => (
+      <div className="card shadow-md card-sm bg-base-100 p-4" key={index}>
+        {skeleton({ widthCls: 'w-10/12', heightCls: 'h-4', className: 'mb-2' })}
+        {skeleton({ widthCls: 'w-8/12', heightCls: 'h-3', className: 'mb-2' })}
+        {skeleton({ widthCls: 'w-full', heightCls: 'h-3', className: 'mb-2' })}
+        {skeleton({ widthCls: 'w-4/12', heightCls: 'h-3' })}
+      </div>
+    ));
+  };
 
-  if (articles.length === 0) {
-    return null; // Eğer hiç makale yoksa bu bölümü hiç gösterme
+  if (!loading && articles.length === 0) {
+    return null;
   }
 
   return (
@@ -40,24 +54,32 @@ const ScholarCard = () => {
             </div>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            {articles.map((item, index) => (
-              <a
-                className="card shadow-md card-sm bg-base-100 cursor-pointer p-4 transition-transform transform hover:scale-105"
-                key={index}
-                href={item.link}
-                target="_blank"
-                rel="noreferrer"
-              >
-                <h3 className="font-semibold text-base-content mb-1">{item.title}</h3>
-                <p className="text-xs text-base-content opacity-60 mb-1">{item.authors}</p>
-                <p className="text-sm text-base-content opacity-80 mb-2">{item.publication}</p>
-                {item.cited_by?.value > 0 && (
-                  <span className="text-xs font-semibold opacity-50">
-                    {item.cited_by.value} atıf
-                  </span>
-                )}
-              </a>
-            ))}
+            {loading
+              ? renderSkeleton()
+              : articles.map((item, index) => (
+                  <a
+                    className="card shadow-md card-sm bg-base-100 cursor-pointer p-4 transition-transform transform hover:scale-105"
+                    key={index}
+                    href={item.link}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <h3 className="font-semibold text-base-content mb-1">
+                      {item.title}
+                    </h3>
+                    <p className="text-xs text-base-content opacity-60 mb-1">
+                      {item.authors}
+                    </p>
+                    <p className="text-sm text-base-content opacity-80 mb-2">
+                      {item.publication}
+                    </p>
+                    {item.cited_by?.value > 0 && (
+                      <span className="text-xs font-semibold opacity-50">
+                        {item.cited_by.value} atıf
+                      </span>
+                    )}
+                  </a>
+                ))}
           </div>
         </div>
       </div>
