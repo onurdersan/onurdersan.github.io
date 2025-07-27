da23091dc3c5205050abb34fd515911a8b46469f
Derşan Onur <65139156+onurdersan@users.noreply.github.com>
2025-07-27 21:35:56 +0300

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -2,14 +2,14 @@
 
 const CONFIG = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (This is the only required config)
+    username: 'onurdersan', // Your GitHub org/user name. (This is the only required config)
   },
   /**
    * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
    * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
    * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
    */
-  base: '/gitprofile/',
+  base: '/',
   projects: {
     github: {
       display: true, // Display GitHub projects?
@@ -51,120 +51,169 @@ const CONFIG = {
       ],
     },
   },
-  seo: { title: 'Portfolio of Ariful Alam', description: '', imageURL: '' },
+  seo: { title: 'Derşan Onur', description: 'Pediatrician, M.D.', imageURL: ''  },
   social: {
-    linkedin: 'ariful-alam',
-    x: 'arif_szn',
-    mastodon: 'arifszn@mastodon.social',
-    researchGate: '',
+    linkedin: 'dersanonur',
+    x: 'dersanonur',
+    mastodon: '',
+    researchGate: 'dersanonur',
     facebook: '',
-    instagram: '',
+    instagram: 'dersanonur',
     reddit: '',
     threads: '',
     youtube: '', // example: 'pewdiepie'
     udemy: '',
     dribbble: '',
     behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
+    medium: 'dersanonur',
+    dev: '',
     stackoverflow: '', // example: '1/jeff-atwood'
     discord: '',
     telegram: '',
-    website: 'https://www.arifszn.com',
+    website: 'https://dersanonur.bio.link',
     phone: '',
-    email: 'arifulalamszn@gmail.com',
+    email: 'drdersanonur@gmail.com',
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      '', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
-    'PHP',
-    'Laravel',
-    'JavaScript',
-    'React.js',
-    'Node.js',
-    'Nest.js',
-    'MySQL',
-    'PostgreSQL',
+    'Pediatrics',
+    'Pediatric Palliative Care',
+    'Pediatric Emergency Medicine',
+    'Pediatrics 4.0',
+    'e-Health and e-Learning',
+    'Data Analysis (Jamovi, R, SPSS, BlueSky)',
+    'Research and Methodology',
+    'Flutter',
     'Git',
-    'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
   ],
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
+      company: 'S.B.Ü. İzmir Tepecik Eğitim ve Araştırma Hastanesi',
+      position: 'Pediatrician',
+      from: 'February 2023',
       to: 'Present',
-      companyLink: 'https://example.com',
+      companyLink: 'https://tepecikeah.saglik.gov.tr/',
     },
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
+      company: 'S.B.Ü. Dr. Behçet Uz Çocuk Hastalıkları ve Cerrahisi E.A.H.',
+      position: 'Pediatrician',
+      from: '2023',
+      to: '2024',
+      companyLink: 'https://behcetuzch.saglik.gov.tr/',
     },
   ],
   certifications: [
     {
-      name: 'Lorem ipsum',
-      body: 'Lorem ipsum dolor sit amet',
-      year: 'March 2022',
-      link: 'https://example.com',
+      name: 'Good Medical Researcher',
+      body: '',
+      year: '',
+      link: '',
+    },
+    {
+      name: 'ICH Good Clinical Practice',
+      body: '',
+      year: '',
+      link: '',
+    },
+      {
+      name: 'Good Clinical Practice Training for Social and Behavioral Research',
+      body: '',
+      year: '',
+      link: '',
+    },
+        {
+      name: 'Scale Development and Adaptation Training',
+      body: '',
+      year: '',
+      link: '',
+    },
+        {
+      name: 'Medical Research Methods',
+      body: '',
+      year: '',
+      link: '',
+    },
+        {
+      name: 'Research Ethics',
+      body: '',
+      year: '',
+      link: '',
+    },
+        {
+      name: 'Bibliometric Analysis',
+      body: '',
+      year: '',
+      link: '',
+    },
+    {
+    name: 'BTK Akademi',
+    body: `a. Project Management Fundamentals
+          b. Project and Risk Management
+          c. Algorithm Design
+          d. Entrepreneurship Fundamentals
+          e. Data Science
+          f. Artificial Intelligence
+          g. Information Technology
+          h. Deep Learning
+          i. Natural Language Processing
+          j. Reinforcement Learning`,
+    year: '',
+    link: '',
     },
   ],
   educations: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
+      institution: 'S.B.Ü. Dr. Behçet Uz Çocuk Hastalıkları ve Cerrahisi E.A.H.',
+      degree: 'Pediatric Specialist',
+      from: '2016',
+      to: '2020',
     },
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2012',
-      to: '2014',
+      institution: 'Dokuz Eylül Üniversitesi Tıp Fakültesi',
+      degree: 'MD',
+      from: '2008',
+      to: '2015',
     },
   ],
   publications: [
     {
-      title: 'Publication Title',
+      title: 'Pediatrics 4.0: the Transformative Impacts of the Latest Industrial Revolution on Pediatrics',
       conferenceName: '',
-      journalName: 'Journal Name',
-      authors: 'John Doe, Jane Smith',
-      link: 'https://example.com',
+      journalName: 'Health Care Analysis',
+      authors: 'Derşan Onur, Çağla Özbakır',
+      link: 'https://doi.org/10.1007/s10728-025-00536-z',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'Industry 4.0 represents the latest phase of industrial evolution, characterized by the seamless integration of cyber-physical systems, the Internet of Things, big data analytics, artificial intelligence, advanced robotics, and cloud computing, enabling smart, adaptive, and interconnected processes where physical, digital, and biological realms converge. In parallel, healthcare has progressed from the traditional, physician-centered model of Healthcare 1.0 by introducing medical devices and digitized records to Healthcare 4.0, which leverages Industry 4.0 technologies to create personalized, data-driven, and patient-centric systems. In this context, we hereby introduce Pediatrics 4.0 as a new paradigm that adapts these innovations to children’s unique developmental, physiological, and ethical considerations and aims to improve diagnostic precision, treatment personalization, and continuous monitoring in pediatric populations. Key applications include AI-driven diagnostic and predictive analytics, IoT-enabled remote monitoring, big data-powered epidemiological insights, robotic assistance in surgery and rehabilitation, and 3D printing for patient-specific devices and pharmaceuticals. However, realizing Pediatrics 4.0 requires addressing significant challenges—data privacy and security, algorithmic bias, interoperability and standardization, equitable access, regulatory alignment, the ethical complexities of consent, and long-term technology exposure. Future research should focus on explainable AI, pediatric-specific device design, robust data governance frameworks, dynamic ethical and legal guidelines, interdisciplinary collaboration, and workforce training to ensure these transformative technologies translate into safer, more effective, and more equitable child healthcare.',
     },
     {
-      title: 'Publication Title',
-      conferenceName: 'Conference Name',
-      journalName: '',
-      authors: 'John Doe, Jane Smith',
-      link: 'https://example.com',
+      title: 'Evaluation of serum vitamin B12 and D, iron, ferritin, folate, calcium, phosphorus and magnesium levels in children in palliative care clinic: a single-center cross-sectional study',
+      conferenceName: '',
+      journalName: 'BMC Palliative Care',
+      authors: 'Derşan Onur, Sunanur Çiftçi Sadıkoğlu, Nilgün Harputluoğlu, Behzat Özkan',
+      link: 'https://doi.org/10.1186/s12904-024-01546-9',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        `Background: Pediatric palliative care (PPC) patients are at an elevated risk of malnutrition. Nutritional inadequacy can also cause micronutrient deficiencies. These factors can lead to weight loss, stunted growth, and poor quality of life. Despite the prevalence of these issues, limited research exists in the micronutrient status of PPC patients. The purpose of this study was to determine the vitamin B12 and D, iron, ferritin, folate, calcium, phosphorus, and magnesium levels of PPC patients to contribute to a better understanding of their micronutrient needs as well as the appropriate management of diet and treatment approaches.
+        Methods: This was a single-center observational cross-sectional retrospective study. This study evaluated the levels of vitamin B12, 25-hydroxyvitamin D, iron, ferritin, folate, calcium, phosphorus, and magnesium in PPC patients. The patients were classified according to the Chronic Complex Conditions (CCC) v2 and then compared.
+        Results: A total of 3,144 micronutrient data points were collected from 822 hospitalizations of 364 patients. At least one micronutrient deficiency was identified in 96.9% of the patients. The most prevalent deficiencies were observed for iron, calcium, and phosphate. In addition, 25-hydroxyvitamin D deficiency was observed in one-third of patients. Calcium, magnesium, phosphorus, folate, and 25-hydroxyvitamin D were negatively correlated with age.
+        Conclusion: The results of this study indicate that micronutrient deficiencies are highly prevalent in PPC patients. These findings have the potential to contribute to improvements in the nutritional and therapeutic management of patients.`,
     },
   ],
   // Display articles from your medium or dev account. (Optional)
   blog: {
     source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
+    username: 'dersanonur', // to hide blog section, keep it empty
     limit: 2, // How many articles to display. Max is 10.
   },
   googleAnalytics: {
-    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+    id: 'G-VNHJYSWV6Y', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: { id: '', snippetVersion: 6 },
   themeConfig: {
-    defaultTheme: 'lofi',
+    defaultTheme: 'winter',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
