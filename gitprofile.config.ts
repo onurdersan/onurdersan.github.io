8b5b8098ecd4de36f6145be8756c45a269ed2102
Derşan Onur <65139156+onurdersan@users.noreply.github.com>
2025-08-26 20:29:03 +0300

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -287,4 +287,13 @@ const CONFIG = {
       'procyon',
     ],
   },
+    // Optional Footer. Supports plain text or HTML.
+  footer: `Made with <a 
+      class="text-primary" href="https://github.com/arifszn/gitprofile"
+      target="_blank"
+      rel="noreferrer"
+    >GitProfile</a> and ❤️`,
+
+  enablePWA: true,
+};
 export default CONFIG;
