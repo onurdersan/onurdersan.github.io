b508780d83f35d6eaeaed06fb15cfdad308e6c5c
Derşan Onur <65139156+onurdersan@users.noreply.github.com>
2025-09-06 12:38:47 +0300

    Update index.tsx

--- src/components/certification-card/index.tsx
+++ src/components/certification-card/index.tsx
@@ -20,7 +20,7 @@ const ListItem = ({
     <div className="font-medium">
         {name}
     </div>
-    <h3 className="mb-4 font-normal">{body}</h3>
+    <h3 className="mb-4 font-normal whitespace-pre-line">{body}</h3>
   </li>
 );
 
